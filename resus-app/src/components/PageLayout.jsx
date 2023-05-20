6267fbd80cc2f92f10681de8164f9f800a164898
Jamesy <jd1457@hotmail.co.uk>
2023-05-20 15:50:38 +0100

    wip

--- resus-app/src/components/PageLayout.jsx
+++ resus-app/src/components/PageLayout.jsx
@@ -1,12 +1,35 @@
 
 import logo from '../img/logo.jpg';
+import { Col, Button, Row } from 'react-bootstrap';
+import PageTypes from '../constants/PageTypes';
 
-const PageLayout = ({children}) => (
-<div>
-    <img className="float-end" alt="logo" src={logo}/>
-    {children}
-</div>
+const PageLayout = ({children, pageType, setPageType}) => {
+    const goBack = () => setPageType(pageType === PageTypes.CONTENT ? PageTypes.CATEGORY : PageTypes.HOME);
+    const goHome = () => setPageType(PageTypes.HOME);
+
+return(
+    <div>
+        <Row>
+            <Col>
+                <Col xs="6">
+                    {pageType !== PageTypes.HOME && 
+                    <>
+                        <Button variant="primary" onClick={() => goHome()}><h2>Home</h2></Button>
+                        <Button variant="primary" onClick={() => goBack()}><h2>Back</h2></Button>
+                    </>
+                    }
+                </Col>
+            </Col>
+            <Col>
+                <div>
+                    <img className="m-1 float-end" alt="logo" src={logo}/>
+                </div>
+            </Col>
+        </Row>
+        {children}
+    </div>
 );
+};
 
 export default PageLayout;
 
