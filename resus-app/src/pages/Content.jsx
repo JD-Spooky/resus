e4ef8f01d88a9890880d27c789c0cb40fa0fd5b0
Jamesy <jd1457@hotmail.co.uk>
2023-05-21 11:24:44 +0100

    finish

--- resus-app/src/pages/Content.jsx
+++ resus-app/src/pages/Content.jsx
@@ -3,18 +3,23 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import Data from '../data/Data';
 import ReactMarkdown from 'react-markdown'
+import { Alert, Container } from 'react-bootstrap';
 
 const Content = ({
     selectedCategory,
     selectedContent,
     setPageType
 }) => {
-
+    const [isError, setIsError] = useState(false);
     const [contentData, setContentData] = useState(false);
     const [markdown, setMarkdown] = useState('');
     useEffect(() => {
+        setIsError(false);
         const cd = Data.find((d) => d.name === selectedCategory);
         const contD = cd.categories.find((c) => c.name === selectedContent);
+        if(contD.content.length === 0){
+            setIsError(true);
+        }
         fetch(contD.content)
         .then (response => response.text())
         .then (text => {
@@ -25,7 +30,16 @@ const Content = ({
     }, [selectedCategory, selectedContent]);
     return (
         <div>
-            {markdown && 
+            {isError && 
+            <Container>
+                <Alert variant="danger">
+                    <span>
+                        <b>Error</b> - file not found!<br />
+                        Please report any issues <a href="https://github.com/JD-Spooky/resus/issues">here</a>
+                    </span>
+                </Alert>
+            </Container>}
+            {!isError && markdown && 
              <ReactMarkdown children={markdown}/>
             }
         </div>
